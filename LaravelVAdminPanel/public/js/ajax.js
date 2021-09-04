ad63d93138b9dca386fb8d4c374b7786fe036855
Zavhorodnii <devoir98@gmail.com>
2021-09-04 13:16:21 +0300

    merge from develo_2

---
+++ LaravelVAdminPanel/public/js/ajax.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./resources/js/ajax.js ***!
+  \******************************/
+$(document).ready(function () {
+  console.log('ajax.js ready');
+});
+/******/ })()
+;
